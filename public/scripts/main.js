029c9b5f6656295cc0765a631eec7bf8bb70985c
jajsmith <j.jaydez@gmail.com>
2016-09-17 17:34:07 -0400

    Add Authentication with google/firebase

---
+++ public/scripts/main.js
@@ -0,0 +1,113 @@
+'use strict';
+
+function Dunia() {
+	this.checkSetup();
+
+	// Shortcuts to DOM Elements.
+  //this.messageList = document.getElementById('messages');
+  //this.messageForm = document.getElementById('message-form');
+  //this.messageInput = document.getElementById('message');
+  //this.submitButton = document.getElementById('submit');
+  //this.submitImageButton = document.getElementById('submitImage');
+  //this.imageForm = document.getElementById('image-form');
+  //this.mediaCapture = document.getElementById('mediaCapture');
+  this.userPic = document.getElementById('user-pic');
+  this.userName = document.getElementById('user-name');
+  this.signInButton = document.getElementById('sign-in');
+  this.signOutButton = document.getElementById('sign-out');
+  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+
+  // Saves message on form submit.
+  //this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+  this.signOutButton.addEventListener('click', this.signOut.bind(this));
+  this.signInButton.addEventListener('click', this.signIn.bind(this));
+
+  // Toggle for the button.
+  //var buttonTogglingHandler = this.toggleButton.bind(this);
+  //this.messageInput.addEventListener('keyup', buttonTogglingHandler);
+  //this.messageInput.addEventListener('change', buttonTogglingHandler);
+
+  // Events for image upload.
+  //this.submitImageButton.addEventListener('click', function() {
+  //  this.mediaCapture.click();
+  //}.bind(this));
+  //this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+  this.initFirebase();
+}
+
+//
+// CONSTANTS
+//
+
+Dunia.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
+
+
+//
+// PROTOTYPE FUNCTIONS
+//
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+Dunia.prototype.initFirebase = function() {
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Signs-in Dunia
+Dunia.prototype.signIn = function(googleUser) {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);};
+
+// Signs-out of Dunia
+Dunia.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+Dunia.prototype.onAuthStateChanged = function(user) {
+  if (user) { // User is signed in!
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL;   // Get profile pic.
+    var userName = user.displayName;        // Get user's name.
+
+    // Set the user's profile pic and name.
+    this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+    this.userName.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    this.userName.removeAttribute('hidden');
+    this.userPic.removeAttribute('hidden');
+    this.signOutButton.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    this.signInButton.setAttribute('hidden', 'true');
+
+    // We load currently existing chant messages.
+    // this.loadMessages();
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.userName.setAttribute('hidden', 'true');
+    this.userPic.setAttribute('hidden', 'true');
+    this.signOutButton.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+  }
+};
+
+
+// Let's make sure the Firebase SDK is working properly
+Dunia.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('Firebase SDK not configured and imported');
+  }
+};
+
+window.onload = function() {
+  window.dunia = new Dunia();
+};
