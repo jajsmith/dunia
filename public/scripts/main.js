005a8e3d8039eb80c59fff055aa1c9736999ba2c
jajsmith <j.jaydez@gmail.com>
2016-09-17 22:19:04 -0400

    Fix script malfunctioning because of undeclared variable

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,12 +5,12 @@ function Dunia() {
 
 	// Shortcuts to DOM Elements.
   this.visitedList = document.getElementById('visited');
-  //this.messageForm = document.getElementById('message-form');
-  //this.messageInput = document.getElementById('message');
-  //this.submitButton = document.getElementById('submit');
-  //this.submitImageButton = document.getElementById('submitImage');
-  //this.imageForm = document.getElementById('image-form');
-  //this.mediaCapture = document.getElementById('mediaCapture');
+  this.messageForm = document.getElementById('message-form');
+  this.messageInput = document.getElementById('message');
+  this.submitButton = document.getElementById('submit');
+  this.submitImageButton = document.getElementById('submitImage');
+  this.imageForm = document.getElementById('image-form');
+  this.mediaCapture = document.getElementById('mediaCapture');
   this.userPic = document.getElementById('user-pic');
   this.userName = document.getElementById('user-name');
   this.signInButton = document.getElementById('sign-in');
